40f78ae08d075f0d27605d4dec145345eb11fbfb
vinvui <vincent.vuillemin@gmail.com>
2025-02-07 19:14:13 +0100

    Upgrade to Laravel 11 + Some clean

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
@@ -14,8 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
         User::create([
             'name' => 'Mia G.',
             'email' => 'mgouget@protonmail.com',
